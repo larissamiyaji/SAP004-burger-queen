3b6d020036ab6900361d3e7731448a4070931d0a
Luana Andrade <luana.las21@gmail.com>
2020-07-17 14:59:50 -0300

    configurado o firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCWwRiN3doLWny1igDXSFz6sVyVFzfqYGE",
+    authDomain: "burger-queen04.firebaseapp.com",
+    databaseURL: "https://burger-queen04.firebaseio.com",
+    projectId: "burger-queen04",
+    storageBucket: "burger-queen04.appspot.com",
+    messagingSenderId: "285508123474",
+    appId: "1:285508123474:web:b942369079f8d0dcc8188b",
+    measurementId: "G-97X0XWW1P4"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  export default firebase;
