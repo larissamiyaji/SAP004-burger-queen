897b427dc0d58e8298b4ededd11ca0a91b1ff2b1
larissamiyaji <natsumi-23@hotmail.com>
2020-07-21 10:32:53 -0300

    Troca de for por htmlFor

--- src/components/login.js
+++ src/components/login.js
@@ -6,10 +6,10 @@ const Login = () => (
     <div className='login'>
         <h2 >Login</h2>
         <form>
-        <label for="email">E-mail:</label><br></br>
+        <label htmlFor="email">E-mail:</label><br></br>
         	<input type="text"  name="email"></input><br></br>
         	<br></br>
-        	<label for="password">Senha:</label><br></br>
+        	<label htmlFor="password">Senha:</label><br></br>
         	<input type="text"  name="password"></input>
         	<br></br>
         	<input type="submit" value="Submit"></input>
