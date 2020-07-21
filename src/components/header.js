decb114130eaf2a18bcfffa8b834a433028a49b2
larissamiyaji <natsumi-23@hotmail.com>
2020-07-21 20:02:05 -0300

    Estilização

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react";
 
 const Header = () => (
   
-      <h1 className="main-title">Burguer Queen</h1>
+      <h1 className="main-title">Burger Queen</h1>
   
 );
 
--- src/components/infoBox.js
+++ src/components/infoBox.js
@@ -5,7 +5,7 @@ import '../App.css';
 const InfoBox = () => (
   <div className='infobox'>
     <section className="description">
-      <h3>Sobre Burguer Queen</h3>
+      <h3>Sobre Burger Queen</h3>
       <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria
         tipográfica e de impressos, e vem sendo utilizado desde o século
         XVI</p>
