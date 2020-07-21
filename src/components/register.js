9bdcffd6bdc81b35f460dd4c6941f04708f27903
larissamiyaji <natsumi-23@hotmail.com>
2020-07-21 10:41:12 -0300

    Troca de for por htmlFor

--- src/components/register.js
+++ src/components/register.js
@@ -6,23 +6,22 @@ const Login = () => (
     <div className='login'>
         <h2 >Cadastro</h2>
         <form >
-        <label for="name">Nome:</label><br></br>
-         <input type="text"  name="name"></input><br></br>
+        <label htmlFor="name">Nome:</label><br></br>
+         <input type="text"  name="name" /><br></br>
          <br></br>
-        <label for="email">E-mail:</label><br></br>
-         <input type="text"  name="email"></input><br></br>
+        <label htmlFor="email">E-mail:</label><br></br>
+         <input type="text"  name="email" /><br></br>
          <br></br>
-         <label for="password">Senha:</label><br></br>
-         <input type="text"  name="password"></input>
+         <label htmlFor="password">Senha:</label><br></br>
+         <input type="text"  name="password" />
          <br></br>
-         <input type="submit" value="Submit"></input>
+         <input type="submit" value="Submit" />
          <br></br>
-         <input type="radio" name="hall" value="hall">
-         <label for="hall">Salão</label><br></br>
-         <input type="radio" name="kitchen" value="kitchen">
-         <label for="kitchen">Cozinha</label><br></br>
+         <input type="radio" name="hall" value="hall" />
+         <label htmlFor="hall">Salão</label><br></br>
+         <input type="radio" name="kitchen" value="kitchen" />
+         <label htmlFor="kitchen">Cozinha</label><br></br>
         </form>
-       
     </div>
 );
 
