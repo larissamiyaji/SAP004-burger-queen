b4f7436f32f5ea0af9ee677ea05816944c19cfcf
larissamiyaji <natsumi-23@hotmail.com>
2020-07-23 19:24:46 -0300

    Criação do layout do modal

--- src/components/hall.js
+++ src/components/hall.js
@@ -1,20 +1,33 @@
 import React from "react";
 import "../App.css";
 
-
 const sendOrder = (event) => {
-    event.preventDefault();
-    console.log('Finalizando pedido');
+  event.preventDefault();
+  console.log("Finalizando pedido");
 };
 
 const cancelOrder = (event) => {
-    event.preventDefault();
-    console.log('Cancelando pedido');
+  event.preventDefault();
+  console.log("Cancelando pedido");
 };
+
+/* ABRE UM POP UP QUANDO CLICAR EM HAMBÚRGUER  
+  const chooseBurger = (event) => {
+    event.preventDefault();
+    console.log("Turbinando Hambúguer");
+}; */
+
+{
+  /* const showPopup = (event) => {
+  event.preventDefault();
+  Modal.visibility = hidden;
+  Modal.style = none;
+};  */
+}
 
 const Hall = () => (
   <div className="hall-template">
-{/* CARDÁPIO */}
+    {/* CARDÁPIO */}
     <section className="text-align menu-all">
       <h2 className="menu-title">Café da Manhã</h2>
       <div className="menu-list">
@@ -42,16 +55,30 @@ const Hall = () => (
       </div>
     </section>
 
-{/*    CARD DE DETALHES DO PEDIDO */}
+    {/*    CARD DE DETALHES DO PEDIDO */}
     <section className="order-card">
       <h2 className="menu-title text-align">Detalhes do Pedido</h2>
       <div className="order-details">
         <p>Nº do pedido: {}</p>
         <p>
-          Nº da mesa: <input className="order-details-input input" placeholder="Insira o nº da mesa" required></input>
+          Nº da mesa:{" "}
+          <input
+            id="table-number"
+            type="number"
+            className="order-details-input input"
+            placeholder="Insira o nº da mesa"
+            required
+          ></input>
         </p>
         <p>
-          Cliente: <input className="order-details-input input" placeholder="Insira o nome do cliente" required></input>
+          Cliente:{" "}
+          <input
+            id="client-name"
+            type="text"
+            className="order-details-input input"
+            placeholder="Insira o nome do cliente"
+            required
+          ></input>
         </p>
       </div>
       <div className="menu-list text-align">
@@ -62,9 +89,64 @@ const Hall = () => (
       </div>
       <div className="order-bottom-info">
         <p className="value-total">Total: R${}</p>
-        <button type="submit" className="form-button cancel-button" onClick={cancelOrder}>Cancelar</button>
-        <button type="submit" className="form-button send-order-button" onClick={sendOrder}>Finalizar</button>
+        <button
+          type="submit"
+          className="form-button cancel-button"
+          onClick={cancelOrder}
+        >
+          Cancelar
+        </button>
+        <button
+          type="submit"
+          className="form-button send-order-button"
+          onClick={sendOrder}
+        >
+          Finalizar
+        </button>
       </div>
+    </section>
+
+    {/* CARD DE TURBINAR O HAMBÚRGUER  */}
+    <section className="modal">
+      <h2>Turbine seu hambúrguer</h2>
+      <h3 className="edit-burger">Tipo de carne</h3>
+      <div className="radio-buttons">
+        <label>
+          <input
+            type="radio"
+            name="meat-type"
+            value="Bovino"
+            checked={true}
+          ></input>
+          Bovino
+        </label>
+        <label>
+          <input type="radio" name="meat-type" value="Frango"></input>Frango
+        </label>
+        <label>
+          <input type="radio" name="meat-type" value="Vegetariano"></input>
+          Vegetariano
+        </label>
+      </div>
+      <h3 className="edit-burger">Adicionais</h3>
+      <div className="menu-list cheese-egg">
+        <p>Queijo _____________ R$1,00</p>
+        <p>Ovo _____________ R$1,00</p>
+      </div>
+      <button
+        type="submit"
+        className="form-button cancel-button"
+        onClick={cancelOrder}
+      >
+        Cancelar
+      </button>
+      <button
+        type="submit"
+        className="form-button send-order-button"
+        onClick={sendOrder}
+      >
+        Finalizar
+      </button>
     </section>
   </div>
 );
