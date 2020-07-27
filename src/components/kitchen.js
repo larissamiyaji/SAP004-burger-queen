d88392ce4d8c5aba59b73725972a7ad4fa1c82f7
larissamiyaji <natsumi-23@hotmail.com>
2020-07-27 15:39:15 -0300

    Implementação da função e botão de Logout

--- src/components/kitchen.js
+++ src/components/kitchen.js
@@ -1,22 +1,14 @@
 import React, { Component } from "react";
-import '../App.css';
-import Button from './Button';
-
-
+import "../App.css";
 
 class Kitchen extends Component {
-    
-    render()
-    {
-        const returnPage = (event) => {
-            event.preventDefault();
-            window.location.href = "/";
-          };
-        return(
-          <div><h1>Logou</h1>
-          <Button onClick={returnPage} name="voltar" type="submit" />
-    </div> 
-        )
-}}
+  render() {
+    return (
+      <div>
+        <h1>Logou</h1>
+      </div>
+    );
+  }
+}
 
 export default Kitchen;
