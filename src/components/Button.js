b10fa4e13be9bf5f3d75b139cbc30dcb3c41e252
larissamiyaji <natsumi-23@hotmail.com>
2020-07-27 15:38:23 -0300

    Implementação do função de Logout

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,25 @@
 import React from "react";
-import '../App.css';
+import "../App.css";
+import firebase from "../firebase"
 
+export const ButtonLogout = () => {
+  const logout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        window.location.href = "/";
+        console.log("Logging out");
+      });
+  };
+  return <button className="form-button" onClick={logout}>Logout</button>;
+};
 
 const Button = (props) => {
-  return <button className="form-button" onClick={props.onClick} id={props.id} >{props.name} {props.title}</button>
-}
+  return (
+    <button className="form-button" onClick={props.onClick} id={props.id}>
+      {props.name} {props.title}
+    </button>
+  );
+};
 export default Button;
