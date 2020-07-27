570bd7f32327d8408737a2854b335152d9ba2fdc
larissamiyaji <natsumi-23@hotmail.com>
2020-07-27 15:06:26 -0300

    Correção da função login - Funcionando 100%

--- src/components/infoBox.js
+++ src/components/infoBox.js
@@ -1,17 +1,31 @@
 import React from "react";
-import '../App.css';
-
+import "../App.css";
 
 const InfoBox = () => {
   return (
-  <div className='infobox'>
-    <section className="description">
-      <h3>Sobre Burger Queen</h3>
-      <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria
-        tipográfica e de impressos, e vem sendo utilizado desde o século
-        XVI</p>
-    </section>
-  </div>
-);
-}
+    <div className="infobox">
+      <section className="description">
+        <div className="burger-queen-description">
+          <h3>Sobre Burger Queen</h3>
+          <p>
+            Lorem Ipsum é simplesmente uma simulação de texto da indústria
+            tipográfica e de impressos, e vem sendo utilizado desde o século XVI
+          </p>
+        </div>
+        {/* <div className="kitchen-hall">
+          <h3>Cozinha</h3>
+          <p>
+            Para ser direcionado para a página da Cozinha, selecione cozinha nas
+            opções acima.
+          </p>
+          <h3>Salão</h3>
+          <p>
+            Para ser direcionado para a página do Salão, selecione salão nas
+            opções acima.
+          </p>
+        </div> */}
+      </section>
+    </div>
+  );
+};
 export default InfoBox;
